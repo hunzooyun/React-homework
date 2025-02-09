709bdd42c853229f9ae586af71f6e510ff105c18
hunzooyun <hunzooyun@gmail.com>
2025-02-09 17:38:14 +0900

    tailwindcss 적용 및 svg 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './tailwind.css';
 import App from '@/app';
 
 const root = document.getElementById('root');
